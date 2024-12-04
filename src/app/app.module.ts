a9d3966dec2f11203ea6af19748b5a0a2712f028
dtuan <tuandtit@gmail.com>
2024-12-04 14:44:07 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,38 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { OrderComponent } from './order/order.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
+import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    DetailProductComponent,
+    OrderComponent,
+    OrderConfirmComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    HomeComponent,
+    DetailProductComponent,
+    OrderComponent,
+    OrderConfirmComponent,
+    LoginComponent,
+    RegisterComponent
+  ]
 })
 export class AppModule { }
